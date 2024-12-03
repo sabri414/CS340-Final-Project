7c21ea2f1dc9306bc1ffea3200fec6c3baf78cf1
sabri414 <133167386+sabri414@users.noreply.github.com>
2024-12-03 21:29:46 +0000

    new changes adding files

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+	/* Display Errors */
+	//ini_set('display_errors', 1);
+	//ini_set('display_startup_errors', 1);
+	//error_reporting(E_ALL);
+	
+	
+	
+	
+	
+	mysqli_report(MYSQLI_REPORT_ERROR );
+
+
+	/* Change for your username and password for phpMyAdmin*/
+	define('DB_SERVER', 'classmysql.engr.oregonstate.edu');
+	define('DB_USERNAME', 'cs340_abounozs');
+	define('DB_PASSWORD', '7825');
+	define('DB_NAME', 'cs340_abounozs');
+	 
+	/* Attempt to connect to MySQL database */
+	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	 
+	// Check connection
+	if($link === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+?>
